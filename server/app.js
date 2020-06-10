ed72d2ba4f091582e1a3042ab42c0bd26196f67f
Fadhil Amadan <fadhil.amadan@yahoo.co.id>
2020-06-10 22:30:02 +0700

    improved: change port

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { MONGOURI } = require('./keys');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
