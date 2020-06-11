723baddff8f44bb6f4256ead169cab361f163524
Fadhil Amadan <fadhil.amadan@yahoo.co.id>
2020-06-11 22:24:31 +0700

    added: client side (like, unlike)

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../App';
 
 const Home = () => {
     const [data, setData] = useState([]);
+    const { state } = useContext(UserContext);
 
     useEffect(() => {
         fetch('/allPost', {
@@ -17,6 +19,63 @@ const Home = () => {
                 console.log(err);
             });
     }, []);
+
+    const likePost = (id) => {
+        fetch('/likePost', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + localStorage.getItem('jwt-token'),
+            },
+            body: JSON.stringify({
+                postId: id,
+            }),
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                const newData = data.map((item) => {
+                    if (item._id === result._id) {
+                        return result;
+                    } else {
+                        return item;
+                    }
+                });
+
+                setData(newData);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const unlikePost = (id) => {
+        fetch('/unlikePost', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + localStorage.getItem('jwt-token'),
+            },
+            body: JSON.stringify({
+                postId: id,
+            }),
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                console.log(result);
+                const newData = data.map((item) => {
+                    if (item._id === result._id) {
+                        return result;
+                    } else {
+                        return item;
+                    }
+                });
+
+                setData(newData);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
     return (
         <div className="home">
@@ -34,6 +93,22 @@ const Home = () => {
                             >
                                 favorite
                             </i>
+                            {item.likes.includes(state._id) ? (
+                                <i
+                                    className="material-icons"
+                                    onClick={() => unlikePost(item._id)}
+                                >
+                                    thumb_down
+                                </i>
+                            ) : (
+                                <i
+                                    className="material-icons"
+                                    onClick={() => likePost(item._id)}
+                                >
+                                    thumb_up
+                                </i>
+                            )}
+                            <h6>{item.likes.length} likes</h6>
                             <h6>{item.title}</h6>
                             <p>{item.body}</p>
                             <input type="text" placeholder="add comment" />
